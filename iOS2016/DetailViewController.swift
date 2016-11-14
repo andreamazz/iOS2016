12aee74cef2df6054230cf31cf3d1ded6e816588
Andrea Mazzini <andrea.mazzini@gmail.com>
2016-11-14 14:19:53 +0100

    Add detail view controller segue

---
+++ iOS2016/DetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.swift
+//  iOS2016
+//
+//  Created by Andrea Mazzini on 14/11/2016.
+//  Copyright © 2016 Andrea Mazzini. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+  
+}
