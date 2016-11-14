01e5229087b519692c70903d05b064ecaa8bb54d
Andrea Mazzini <andrea.mazzini@gmail.com>
2016-11-14 14:22:43 +0100

    Add detail view outlets and delegates

--- iOS2016/DetailViewController.swift
+++ iOS2016/DetailViewController.swift
@@ -9,5 +9,27 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-  
+
+  @IBOutlet var tableView: UITableView? {
+    didSet {
+      tableView?.tableFooterView = UIView()
+    }
+  }
+  @IBOutlet var label: UILabel?
+  @IBOutlet var imageView: UIImageView?
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+}
+
+extension DetailViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 0
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    return tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+  }
 }
